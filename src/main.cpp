d0a438be99fcb918a9a2bbe39b7cb580efb572ec
Marco A. Piccolino-Boniforti <marco.a.piccolino@gmail.com>
2016-12-28 09:57:36 +0100

    enable hi-dpi; add application metadata

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine;
+    QGuiApplication::setOrganizationName("QtMob");
+    QGuiApplication::setOrganizationDomain("http://www.qtmob.org");
 
+    QGuiApplication::setApplicationName("PropertyCross-Flux-Atomic");
+    QGuiApplication::setApplicationVersion("1.0.0");
+
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
